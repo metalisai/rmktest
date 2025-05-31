11681d32378ae5ed20e6eb52f0c29a32f1c73ff0
Talis Tammearu <talis995@gmail.com>
2025-05-31 13:13:01 +0300

    bus trajectory fetching

--- tltdata.py
+++ tltdata.py
@@ -1,3 +1,6 @@
+import requests
+import polyline
+
 # gps.txt file format
 # type, lineid, lat, lon, speed, heading, vehicleid, vehicletype
 # type - 1 trolley, 2 bus, 3 tram, 7 nightbus
@@ -15,3 +18,15 @@ def get_bus_locations(file, bus):
                 vehicleid = int(cols[6])
                 results[vehicleid] = (lat, lon, heading)
     return results
+
+def get_bus_trajectory(bus):
+    url = f"https://transport.tallinn.ee/data/tallinna-linn_bus_{bus}.txt"
+    response = requests.get(url)
+    if response.status_code == 200:
+        trajectory_data = response.text
+    else:
+        raise Exception("Failed to fetch trajectory data.")
+    traj_datas = trajectory_data.split("\r\n")
+    traj_ab = polyline.decode(traj_datas[1])
+    traj_ba = polyline.decode(traj_datas[4])
+    return traj_ab, traj_ba
